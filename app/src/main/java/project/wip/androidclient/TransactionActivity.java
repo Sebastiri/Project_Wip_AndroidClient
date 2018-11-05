668394ab38b2df7b4eb58b6313ecb2099fda1f8a
Sebastiri <sebastian.rieck@mailbox.org>
2018-11-05 12:05:41 +0100

    Commit uncommitted important class LogInActivity

---
+++ app/src/main/java/project/wip/androidclient/TransactionActivity.java
@@ -0,0 +1,13 @@
+package project.wip.androidclient;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class TransactionActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_transaction);
+    }
+}
