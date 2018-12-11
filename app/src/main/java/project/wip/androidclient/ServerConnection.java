185b9f1b7400b56cd2a3d251abb9a8a0b2ff5432
Sebastiri <sebastian.rieck@mailbox.org>
2018-12-11 13:08:31 +0100

    Commit a la Arvato 1

--- app/src/main/java/project/wip/androidclient/ServerConnection.java
+++ app/src/main/java/project/wip/androidclient/ServerConnection.java
@@ -13,7 +13,6 @@ import com.google.gson.GsonBuilder;
 import org.apache.http.HttpResponse;
 import org.apache.http.HttpStatus;
 import org.apache.http.NameValuePair;
-import org.apache.http.client.HttpClient;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
